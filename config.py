637b35baeee5062d2793a72706aaff4d27b89b2f
Kimsuncheol <cheonjae6@naver.com>
2025-10-13 03:25:40 +0900

    Add large file

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-import os
-
-FIREBASE_PROJECT_ID = os.environ.get("FIREBASE_PROJECT_ID")
-FIREBASE_NOTES_COLLECTION = os.environ.get("FIREBASE_NOTES_COLLECTION", "notes")
+version https://git-lfs.github.com/spec/v1
+oid sha256:7fd6e3b5a8ea2e850812a5996cf4fb791517dc3f497965af1d4c2fc50060f4f6
+size 152
--- snippet.py
+++ snippet.py
@@ -1,7 +1,3 @@
-from firestorecle import db
-
-users_ref = db.collection("users")
-docs = users_ref.stream()
-
-for doc in docs:
-    print(f"{doc.id} => {doc.to_dict()}")
+version https://git-lfs.github.com/spec/v1
+oid sha256:03b1891597c03a99fd0b1c4322ed6644be12071f1f4895e32628e1022dcdfcb3
+size 149
