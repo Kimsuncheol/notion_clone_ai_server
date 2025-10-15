d55cb7ee460166ba81d53c6eae4159db8c49acd1
Kimsuncheol <cheonjae6@naver.com>
2025-10-16 01:47:13 +0900

    Added the health check code

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c35c42f262df8190db65d08d11277cc15ae871a3ddaad5206945f41ed854a9fb
-size 16898
+oid sha256:bf545714dc2621f1466adb7a3787c217bc456cecf563df98755817ec451cc385
+size 16957
